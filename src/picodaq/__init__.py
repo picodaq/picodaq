45f01bcebe092858dc47f18095ef99d1a0e8e7d0
Daniel A. Wagenaar <daw@caltech.edu>
2025-01-24 15:19:37 -0800

    Added dependency on pyserial

--- src/picodaq/__init__.py
+++ src/picodaq/__init__.py
@@ -3,3 +3,6 @@ from .dac import AnalogOut, DigitalOut
 from . import stimulus
 from .units import Hz, kHz, s, ms, V, mV, Time, Frequency
 from .errors import DeviceError
+from .device import picodaqs
+
+version = "0.1.241024"
